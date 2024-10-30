5a7abdfc510d619a2c349ba6c938b28e06aa8168
HaeRim <yjec14@naver.com>
2024-10-30 17:46:51 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+{/* 주석문을 작성합니다. 
+  작성자: phr
+  작성일: 2024-10-30
+  내용: 기능에 대한 내용*/}
+
 function App() {
+  let name = "React";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className='test'>Hello, {
+          name === "React" ? (<h1>YES</h1>): null
+        }!!</h1>
+      <p>반갑습니다.</p>
     </div>
   );
+
+  // const port = undefined;
+
+  // return(
+  //   <div>
+  //     {
+  //       prot || '3000'
+  //     }
+  //   </div>
+  // )
 }
 
 export default App;
